dcdf745ab19f460189d00f7fa77f07e3ec1da785
gdiazm <gdiaz4@hawk.iit.edu>
2016-02-04 00:40:22 +0100

    Diagonal Difference

--- Warmup-Challenges/DiagonalDifference.java
+++ Warmup-Challenges/DiagonalDifference.java
@@ -8,12 +8,26 @@ public class DiagonalDifference {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int a[][] = new int[n][n];
-        for(int a_i=0; a_i < n; a_i++){
-            for(int a_j=0; a_j < n; a_j++){
-                a[a_i][a_j] = in.nextInt();
+        int N = in.nextInt();
+        int[][] m= new int [N][N];
+        int d1=0;
+        int d2=0;
+        for(int i=0; i<N; i++)
+        {
+            for(int j=0; j<N ; j++)
+            {
+               m[i][j]=in.nextInt();
+               if(i==j)
+               {
+                   d1=d1+m[i][j];
+               }                
+               if(i+j==N-1)
+               {
+                   d2=d2+m[i][j];
+               }
             }
         }
+        
+        System.out.println(Math.abs(d1-d2));
     }
 }
