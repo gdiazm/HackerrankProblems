14c5c100b18a6a9483c5b526ad48ff4cbc54b8e3
gdiazm <gdiaz4@hawk.iit.edu>
2016-02-04 00:40:57 +0100

    Plus Minus

--- Warmup-Challenges/PlusMinus.java
+++ Warmup-Challenges/PlusMinus.java
@@ -10,8 +10,19 @@ public class PlusMinus {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int arr[] = new int[n];
+        int pos=0, neg=0, zero=0;
         for(int arr_i=0; arr_i < n; arr_i++){
             arr[arr_i] = in.nextInt();
+            if(arr[arr_i]<0){
+                neg++;
+            }else if(arr[arr_i]==0){
+                zero++;
+            }else{
+                pos++;
+            }
         }
-    }
+         System.out.println((float)pos/n);
+         System.out.println((float)neg/n);
+         System.out.println((float)zero/n);
+    }     
 }
