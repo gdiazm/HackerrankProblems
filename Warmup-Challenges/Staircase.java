563f2917116eda3f7f68deb4ff2fd774ab147d06
gdiazm <gdiaz4@hawk.iit.edu>
2016-02-04 00:41:35 +0100

    Staircase

--- Warmup-Challenges/Staircase.java
+++ Warmup-Challenges/Staircase.java
@@ -9,5 +9,22 @@ public class Staircase {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        char chars[][]= new char[n][n];
+        for (int i=0; i<n; i++)
+        {
+            for(int j=n-i-1;j<n;j++)
+            {
+                chars[i][j]='#';
+            }
+            for(int k=0;k<n-i-1;k++)
+            {
+                chars[i][k]=' ';
+            }
+        }
+        
+        for(int a=0;a<n;a++)
+        {    
+                System.out.println(chars[a]);
+        }    
     }
 }
