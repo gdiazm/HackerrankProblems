ebcd6ee810a620d5b22fa848dc931e3ce077c584
gdiazm <gdiaz4@hawk.iit.edu>
2016-02-04 00:38:48 +0100

    Simple Array Sum

--- Warmup-Challenges/SimpleArraySum.java
+++ Warmup-Challenges/SimpleArraySum.java
@@ -4,14 +4,16 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class SimpleArraySum {
+public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int arr[] = new int[n];
-        for(int arr_i=0; arr_i < n; arr_i++){
-            arr[arr_i] = in.nextInt();
+        int N = in.nextInt();
+        int sum=0;
+        for(int i=0;i<N;i++)
+        {
+            sum=sum+in.nextInt();
         }
+        System.out.println(sum);
     }
 }
